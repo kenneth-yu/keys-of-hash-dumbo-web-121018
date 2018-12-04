56745da508b780327e7539a91332b68628cb79f4
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-12-04 17:43:36 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+require 'pry'
 class Hash
   def keys_of(arguments)
     # code goes here
+    answer= []
+    self.each do |value|
+      if value[1] == arguments
+        answer.push(self.key(arguments))
+      end
+    end
+    answer
   end
 end
